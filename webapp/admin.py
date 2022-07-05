a9256d0c3830e132aba26c59b6f6abf8b906f8eb
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-05 09:08:13 +0600

    3d commit : registered model in admin.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'remaining_amount', 'price', 'category']
+    list_display_links = ['id']
+    list_filter = ['category']
+    search_fields = ['name', 'id']
+    fields = ['name', 'description', 'remaining_amount', 'price', 'category']
+    readonly_fields = ['price', 'category']
+
+
+admin.site.register(Product, ProductAdmin)
