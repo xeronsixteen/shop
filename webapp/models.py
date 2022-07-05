55630f15c38b0cfd1d847708044c6c27dcc35e0a
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-05 17:28:49 +0600

    15th commit: refactored remain model from Decimal to Positive INteger

--- webapp/models.py
+++ webapp/models.py
@@ -8,13 +8,13 @@ CATEGORY_CHOICES = [('Other', 'Other'), ('Clothes', 'Clothes'), ('Food', 'Food')
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='name', null=False, blank=False)
     description = models.TextField(max_length=2000, verbose_name='description', null=True, blank=True)
-    remaining_amount = models.DecimalField(verbose_name='remain', max_digits=5, decimal_places=5)
+    remain = models.PositiveIntegerField(verbose_name='remain', null=False, blank=False)
     price = models.DecimalField(verbose_name='price', decimal_places=2, max_digits=7)
     category = models.CharField(max_length=50, verbose_name='category', null=False, blank=False,
                                 default=CATEGORY_CHOICES[0][0], choices=CATEGORY_CHOICES)
 
     def __str__(self):
-        f'{self.pk}. {self.name}{self.price}'
+        return '{self.pk}. {self.name}: {self.price}'
 
     class Meta:
         db_table = 'products'
